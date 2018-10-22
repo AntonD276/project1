eb263672d1f01c569a15c1c95e9a851b2c61e3e1
AntonDyadyushev <antond1429@gmail.com>
2018-10-23 01:07:25 +0300

    Message

--- mainPage/mainPage.js
+++ mainPage/mainPage.js
@@ -1,11 +1,8 @@
-var slides = document.querySelectorAll('.slide');
-var currentSlide = 0;
-/*var noise = document.querySelectorAll('.slides .noise');*/
-var slideInterval = setInterval(nextSlide,3000);
-function b(){slides[currentSlide].className = 'slide showing'};
-function nextSlide() {
-    slides[currentSlide].className = 'slide';
-    setTimeout(b,1000);
-    currentSlide = (currentSlide+1)%slides.length;
-
+var listOfSlides = document.querySelectorAll(".slide");
+var i = 0;
+setInterval(changingSlides,3000);
+function changingSlides() {
+    listOfSlides[i].classList.remove('showing');
+    i = ++i % listOfSlides.length;
+    listOfSlides[i].classList.add("showing");
 }
